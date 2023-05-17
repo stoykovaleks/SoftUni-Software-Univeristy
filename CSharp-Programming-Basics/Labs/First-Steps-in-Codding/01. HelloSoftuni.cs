12a58f918288d709537702f974a1096c754a6730
Aleks Stoykov <119664593+stoykovaleks@users.noreply.github.com>
2023-05-17 13:34:26 +0300

    Update and rename 01.Excellent-Result to 01. HelloSoftuni.cs

---
+++ CSharp-Programming-Basics/Labs/First-Steps-in-Codding/01. HelloSoftuni.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _01_Hello_SoftUni_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
